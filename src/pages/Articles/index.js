d9566f9ef70386b8f54870c666ed18e51b6b90a4
wangjchs <wangjchs@yonyou.com>
2020-01-06 16:51:51 +0800

    -

--- src/pages/Articles/index.js
+++ src/pages/Articles/index.js
@@ -1,44 +1,97 @@
 import React from 'react'
-import Content from '@/articles/2019 yearly review.md'
-import { Main, Title, TagBox, Tag, StyledMarkdown, H1, H2, Paragraph, Blockquote } from './style'
+import Markdown from 'markdown-to-jsx'
+import H1 from './components/H1'
+import H2 from './components/H2'
+import Paragraph from './components/Paragraph'
+import Blockquote from './components/Blockquote'
 
-export default class Root extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      content: Content.split('---')
+export default function Root(props) {
+  const { state } = props.location
+  const MAIN = require(`@/articles/${state.name}.md`).default || ''
+  const TAGS = state.tags || []
+  const TITLE = state.name || ''
+  // 为 markdown 组件注入样式
+  const scoped = resolveScopedStyles(
+    <scope>
+      <style jsx>{`
+        .markdown {
+          font-size: 1.2rem;
+          color: #555555;
+          letter-spacing: 1px;
+          cursor: default;
+        }
+      `}</style>
+    </scope>
+  )
+  function resolveScopedStyles(scope) {
+    return {
+      className: scope.props.className,
+      styles: scope.props.children
     }
   }
-  render() {
-    const { content } = this.state
-    const main = content[1]
-    const options = content[0].split('\n')
-    const optionsObject = {}
-    for (let item of options) {
-      if (item) {
-        item = item.split(': ')
-        optionsObject[item[0]] = item[1]
-      }
-    }
-    const tags = (optionsObject.tags || optionsObject.tag || '').split(', ')
-    return (
-      <Main>
-        <Title>{optionsObject.title}</Title>
-        <TagBox>{tags && tags.map((item, index) => <Tag key={index}>{item}</Tag>)}</TagBox>
-        <StyledMarkdown
-          options={{
-            forceBlock: true,
-            overrides: {
-              h1: H1,
-              h2: H2,
-              p: Paragraph,
-              blockquote: Blockquote
-            }
-          }}
-        >
-          {main}
-        </StyledMarkdown>
-      </Main>
-    )
-  }
+
+  return (
+    <main>
+      <header>{TITLE}</header>
+      <div className="tagBox">
+        {TAGS &&
+          TAGS.map((item, index) => (
+            <div className="tag" key={index}>
+              {item}
+            </div>
+          ))}
+      </div>
+      <Markdown
+        className={`markdown ${scoped.className}`}
+        children={MAIN}
+        options={{
+          forceBlock: true,
+          overrides: {
+            h1: H1,
+            h2: H2,
+            p: Paragraph,
+            blockquote: Blockquote
+          }
+        }}
+      />
+      {scoped.styles}
+      <style jsx>{`
+        main {
+          max-width: 1000px;
+          margin: 0 auto;
+          padding-bottom: 5vh;
+        }
+        header {
+          text-align: center;
+          display: block;
+          font-size: 2rem;
+          margin-block-start: 0.67em;
+          margin-block-end: 0.67em;
+          font-weight: bold;
+        }
+        .tagBox {
+          text-align: center;
+          margin: 20px 0;
+        }
+        .tag {
+          display: inline-block;
+          background-color: #e9e9e9;
+          letter-spacing: 1px;
+          border-radius: 5px;
+          font-size: 1.3rem;
+          height: 30px;
+          line-height: 30px;
+          padding: 0 5px;
+          margin: 0 5px;
+          position: relative;
+          cursor: pointer;
+          &:before {
+            content: '#';
+            left: 0;
+            font-size: 1.1rem;
+          }
+        }
+      `}</style>
+    </main>
+  )
 }
--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FeedBox, FeedItem, ArticleTitle, ArticleDate } from './style'
 import { withRouter } from 'react-router-dom'
 import root from '@/articles/root.json'
 
@@ -8,17 +7,76 @@ function Feed(props) {
     props.history.push('/article', root[index])
   }
   return (
-    <FeedBox>
+    <main>
       {root &&
-        root.map((item, index, arr) => (
-          <FeedItem key={index} len={arr.length}>
-            <ArticleTitle onClick={() => handleClick(index)} len={arr.length}>
+        root.map((item, index) => (
+          <article key={index}>
+            <div className="title" onClick={() => handleClick(index)}>
               {item.name}
-            </ArticleTitle>
-            <ArticleDate onClick={() => handleClick(index)}>{item.date}</ArticleDate>
-          </FeedItem>
+            </div>
+            <div className="date" onClick={() => handleClick(index)}>
+              {item.date}
+            </div>
+          </article>
         ))}
-    </FeedBox>
+      <style jsx>{`
+        main {
+          max-width: 800px;
+          margin: 50px auto;
+          padding-bottom: 5vh;
+        }
+        article {
+          display: flex;
+          position: relative;
+          padding-left: 20px;
+          height: 50px;
+          margin: 0 10vw;
+          line-height: 50px;
+          &::before {
+            content: '';
+            position: absolute;
+            width: 2px;
+            height: 100%;
+            left: 0;
+            background-color: #ff7755;
+          }
+          &:first-child::before {
+            top: 50%;
+            height: 50%;
+          }
+          &:last-child::before {
+            height: 50%;
+          }
+        }
+        .title {
+          padding: 0 10px;
+          text-transform: uppercase;
+          position: relative;
+          height: 100%;
+          cursor: pointer;
+          &::before {
+            content: '';
+            background-color: #ff7755;
+            width: 20px;
+            height: 2px;
+            position: absolute;
+            left: -20px;
+            top: 50%;
+          }
+        }
+        .date {
+          cursor: pointer;
+          color: #bbbbbb;
+        }
+      `}</style>
+      <style jsx>{`
+        .title {
+          &::before {
+            block: 
+          }
+        }
+      `}</style>
+    </main>
   )
 }
 
